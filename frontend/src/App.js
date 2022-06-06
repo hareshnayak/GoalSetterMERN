c652b411b69ef4144fb287dc31deb717d6c27b60
hareshnayak <sketchharry01@gmail.com>
2022-06-06 10:13:43 +0530

    initialised frontend with react redux

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>
+        My App Haresh React Redux
+        </h1>     
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {
+  
+    // TODO:
+    // Use goal and user reducer
+
+  },
+});
