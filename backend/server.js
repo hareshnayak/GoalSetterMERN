8866e46d2d5caafd43c45bea89b9b5c3926e2cef
hareshnayak <sketchharry01@gmail.com>
2022-06-08 14:25:31 +0530

    pushing to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()   
@@ -15,6 +16,12 @@ app.use(express.urlencoded({extended: false})) // for urlencoded
 app.use('/api/goals', require('./routes/goal_routes'))
 app.use('/api/users', require('./routes/user_routes'))
 
+//Serve frontend
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(_dirname, '../frontend/build')))
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html' )))
+} 
+  
 app.use(errorHandler) // to override the error handler provided by express
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`))
