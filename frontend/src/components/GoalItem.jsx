9a10b590c884a367c8f36be331365680df3104c3
hareshnayak <sketchharry01@gmail.com>
2022-06-08 14:02:16 +0530

    tried to reload on deleting goal

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,14 +1,19 @@
 import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { deleteGoal } from '../features/goals/goalSlice'
 
 function GoalItem({ goal }) {
+const navigate = useNavigate()
   const dispatch = useDispatch()
-
+    const onClick = () =>{
+        dispatch(deleteGoal(goal._id))
+        navigate('/login')
+    }
   return (
     <div className='goal'>
       <div>{new Date(goal.createdAt).toLocaleString('en-US')}</div>
       <h2>{goal.text}</h2>
-      <button onClick={() => dispatch(deleteGoal(goal._id))} className='close'>
+      <button onClick={onClick} className='close'>
         X
       </button>
     </div>
